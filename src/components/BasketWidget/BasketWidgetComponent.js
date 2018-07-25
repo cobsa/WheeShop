990719be47d85deb5d20693ee121baa58c0103b4
Jaakko Koponen <cobsa89@gmail.com>
2018-07-25 17:08:58 +0300

    Add storybook, svg-loader and required configs

--- src/components/BasketWidget/BasketWidgetComponent.js
+++ src/components/BasketWidget/BasketWidgetComponent.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-import basketLogo from "../../assets/shopping-cart.svg";
+import BasketLogo from "../../assets/shopping-cart.svg";
 import "./BasketWidgetComponent.css";
 
-export default (props) => {
+export default props => {
   return (
     <div className="basket-widget-container">
       <div className="basket-widget-text">{props.text}</div>
-      <img src={basketLogo} alt="Basket" className="basket-svg" />
+      <BasketLogo className="basket-widget-svg"/>
     </div>
   );
 };
